aab802f4dc2a7ed7be7184b5f8a75f47077fdbbe
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-11-16 12:59:27 +0600

    java working good

--- code/main.java
+++ code/main.java
@@ -13,18 +13,19 @@ import java.util.Scanner; // Import the Scanner class
 
 class Main {
       public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+            Scanner input = new Scanner(System.in);
             System.out.println("enter your name");
-            String name = scanner.nextLine();
+            String name = input.nextLine();
             System.out.println("your name is " + name);
 
             System.out.println("Now start math : ");
             System.out.println("Input your First Number");
 
-            int num1 = scanner.nextInt();
-            int num2 = scanner.nextInt();
+            int num1 = input.nextInt();
+            int num2 = input.nextInt();
             int total = num1+num2;
             System.out.println("Your have Total : " + total);
+            input.close();
             
 
       }
