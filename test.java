bd95080bea59e94a0bba6917f07e40a2038fb259
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-11-16 12:54:45 +0600

    Initial commit

---
+++ test.java
@@ -0,0 +1,31 @@
+import java.util.Scanner; // Import the Scanner class
+
+// class Main {
+//   public static void main(String[] args) {
+//     Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+//     System.out.println("Enter username");
+
+//     String userName = myObj.nextLine();  // Read user input
+//     System.out.println("Username is: " + userName);  // Output user input
+//   }
+// }
+
+class Main {
+      public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("enter your name");
+            String name = scanner.nextLine();
+            System.out.println("your name is " + name);
+
+            System.out.println("Now start math : ");
+            System.out.println("Input your First Number");
+
+            int num1 = scanner.nextInt();
+            int num2 = scanner.nextInt();
+            int total = num1+num2;
+            System.out.println("Your have Total : " + total);
+            
+
+      }
+
+}
