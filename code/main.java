cd855570264f77992a87439da9daf290c7088f87
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-11-16 13:17:32 +0600

    working fine

--- code/main.java
+++ code/main.java
@@ -1,15 +1,6 @@
 package code;
+
 import java.util.Scanner; // Import the Scanner class
-
-// class Main {
-//   public static void main(String[] args) {
-//     Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-//     System.out.println("Enter username");
-
-//     String userName = myObj.nextLine();  // Read user input
-//     System.out.println("Username is: " + userName);  // Output user input
-//   }
-// }
 
 class Main {
       public static void main(String[] args) {
@@ -21,12 +12,28 @@ class Main {
             System.out.println("Now start math : ");
             System.out.println("Input your First Number");
 
-            int num1 = input.nextInt();
+            int num = input.nextInt();
             int num2 = input.nextInt();
-            int total = num1+num2;
+            int total = num + num2;
             System.out.println("Your have Total : " + total);
+
+            /// if else with java
+            System.out.println("Now start java if else and elsen if");
+            System.out.println("enter your condion number : ");
+            int num1 = input.nextInt();
+
+            if (num1 < 10) {
+                  System.out.println("num1 is less then 10");
+            } else if (num1 == 10) {
+                  System.out.println("Num1 equal 10");
+            } else {
+                  System.out.println("Num1 is Getter then 10");
+            }
+
+            var totalNummber = total + name;
+            System.out.println("totalVariavle" + totalNummber);
+
             input.close();
-            
 
       }
 
