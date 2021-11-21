8b5e7f0c5c73f6bdb18646ce2057f158825f3528
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-11-21 11:57:34 +0600

    working

--- code/FileWriteReadClass.java
+++ code/FileWriteReadClass.java
@@ -1,47 +1,47 @@
 package code;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.util.Scanner; // Import the Scanner class
-import java.io.FileOutputStream;
-import java.io.FilterOutputStream;
-import java.io.InputStreamReader;
+      import java.io.BufferedReader;
+      import java.io.File;
+      import java.io.FileInputStream;
+      import java.util.Scanner; // Import the Scanner class
+      import java.io.FileOutputStream;
+      import java.io.FilterOutputStream;
+      import java.io.InputStreamReader;
 
-public class FileWriteReadClass {
-      Scanner input = new Scanner(System.in);
+                  public class FileWriteReadClass {
+                        Scanner input = new Scanner(System.in);
 
-      public void getWriteReadFun() {
+                        public void getWriteReadFun() {
 
-            System.out.println("input you documentions for output the result : ");
-            String name = input.nextLine();
-            String filePath = "/Users/developer/Desktop/java/texts.text";
+                              System.out.println("input you documentions for output the result : ");
+                              String name = input.nextLine();
+                              String filePath = "/Users/developer/Desktop/java/texts.text";
 
-            try {
-                  File data = new File(filePath);
-                  FileOutputStream file = new FileOutputStream(data);
-                  FilterOutputStream filter = new FilterOutputStream(file);
+                              try {
+                                    File data = new File(filePath);
+                                    FileOutputStream file = new FileOutputStream(data);
+                                    FilterOutputStream filter = new FilterOutputStream(file);
 
-                  byte b[] = name.getBytes();
-                  System.out.println(b);
-                  filter.write(b);
-                  filter.flush();
-                  filter.close();
-                  file.close();
-                  System.out.println("Success...");
-                  FileInputStream fstream = new FileInputStream(filePath);
-                  BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-                  String strLine;
-                  while ((strLine = br.readLine()) != null) {
-                        System.out.println("You have Result : ");
+                                    byte b[] = name.getBytes();
+                                    System.out.println(b);
+                                    filter.write(b);
+                                    filter.flush();
+                                    filter.close();
+                                    file.close();
+                                    System.out.println("Success...");
+                                    FileInputStream fstream = new FileInputStream(filePath);
+                                    BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+                                    String strLine;
+                                    while ((strLine = br.readLine()) != null) {
+                                          System.out.println("You have Result : ");
 
-                        System.out.println(strLine);
+                                          System.out.println(strLine);
+                                    }
+                                    fstream.close();
+
+                              } catch (Exception e) {
+                                    System.out.println(e);
+                              }
+
+                        }
                   }
-                  fstream.close();
-
-            } catch (Exception e) {
-                  System.out.println(e);
-            }
-
-      }
-}
--- code/main.java
+++ code/main.java
@@ -1,14 +1,51 @@
 package code;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Scanner; // Import the Scanner class
+import java.io.FileOutputStream;
+import java.io.FilterOutputStream;
+import java.io.InputStreamReader;
 
 class Main {
 
       public static void main(String[] args) {
-        FileWriteReadClass fileWriteReadClass = new FileWriteReadClass();
-        System.out.println("Print Line");
-        fileWriteReadClass.getWriteReadFun();
 
+            Scanner input = new Scanner(System.in);
+            System.out.println("input you documentions for output the result : ");
+            String name = input.nextLine();
+            String filePath = "/Users/developer/Desktop/java/texts.text";
 
+            try {
+                  File data = new File(filePath);
+                  FileOutputStream file = new FileOutputStream(data);
+                  FilterOutputStream filter = new FilterOutputStream(file);
+
+                  byte b[] = name.getBytes();
+                  System.out.println(b);
+                  filter.write(b);
+                  filter.flush();
+                  filter.close();
+                  file.close();
+                  System.out.println("Success...");
+                  FileInputStream fstream = new FileInputStream(filePath);
+                  BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+                  String strLine;
+                  while ((strLine = br.readLine()) != null) {
+                        System.out.println("You have Result : ");
+
+                        System.out.println(strLine);
+                  }
+                  fstream.close();
+
+            } catch (Exception e) {
+                  System.out.println(e);
+            }
+
+            // FileWriteReadClass fileWriteReadClass = new FileWriteReadClass();
+            // System.out.println("Print Line");
+            // fileWriteReadClass.getWriteReadFun();
 
             // Scanner input = new Scanner(System.in);
             // System.out.println("enter your name");
