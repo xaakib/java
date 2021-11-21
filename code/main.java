38eca4475842bb5e75e8ccf89d5529991f4f3596
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-11-21 11:49:44 +0600

    whave bug code exicute

--- code/main.java
+++ code/main.java
@@ -1,39 +1,47 @@
 package code;
 
-import java.util.Scanner; // Import the Scanner class
 
 class Main {
+
       public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-            System.out.println("enter your name");
-            String name = input.nextLine();
-            System.out.println("your name is " + name);
+        FileWriteReadClass fileWriteReadClass = new FileWriteReadClass();
+        System.out.println("Print Line");
+        fileWriteReadClass.getWriteReadFun();
 
-            System.out.println("Now start math : ");
-            System.out.println("Input your First Number");
 
-            int num = input.nextInt();
-            int num2 = input.nextInt();
-            int total = num + num2;
-            System.out.println("Your have Total : " + total);
 
-            /// if else with java
-            System.out.println("Now start java if else and elsen if");
-            System.out.println("enter your condion number : ");
-            int num1 = input.nextInt();
+            // Scanner input = new Scanner(System.in);
+            // System.out.println("enter your name");
+            // String name = input.nextLine();
+            // System.out.println("your name is " + name);
 
-            if (num1 < 10) {
-                  System.out.println("num1 is less then 10");
-            } else if (num1 == 10) {
-                  System.out.println("Num1 equal 10");
-            } else {
-                  System.out.println("Num1 is Getter then 10");
-            }
+            // System.out.println("Now start math : ");
+            // System.out.println("Input your First Number");
 
-            var totalNummber = total + name;
-            System.out.println("totalVariavle" + totalNummber);
+            // int num = input.nextInt();
+            // int num2 = input.nextInt();
+            // int total = num + num2;
+            // System.out.println("Your have Total : " + total);
 
-            input.close();
+            // /// if else with java
+            // System.out.println("Now start java if else and elsen if");
+            // System.out.println("enter your condion number : ");
+            // int num1 = input.nextInt();
+
+            // if (num1 < 10) {
+            // System.out.println("num1 is less then 10");
+            // } else if (num1 == 10) {
+            // System.out.println("Num1 equal 10");
+            // } else {
+            // System.out.println("Num1 is Getter then 10");
+            // }
+
+            // var totalNummber = total + name;
+            // System.out.println("totalVariavle" + totalNummber);
+            // input.close();
+            //////
+
+            /////// Java file write and read from user inout and output/////
 
       }
 
